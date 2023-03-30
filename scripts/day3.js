c369f4c5756b185b66671638ae2c1953dd099411
Clairon <niwemfurac@gmail.com>
2023-03-30 17:21:12 +0200

    adding what we studied on javascript on our day3

---
+++ scripts/day3.js
@@ -0,0 +1,38 @@
+
+//  setTimeout(()=>{
+// // window.location.href="https://instagram.com"
+// console.log("clairon");
+//  },5000);
+
+//  setInterval(()=>{
+//     alert("heyyyy");
+//  },4000);
+ 
+//  const clairon= setTimeout(()=>{
+//     // window.location.href="https://instagram.com"
+//     alert("i love u");
+//  },5000);
+// //  clearTimeout(clairon);
+const cl=setInterval(() => {
+    alert("heyy");
+},4000);
+document.getElementById("btn");
+btn.addEventListener("click",()=>{
+    clearInterval(cl);
+});
+
+
+let a="Data types";
+console.log(a);
+let b=2023;
+console.log(b);
+let c="0\\7";
+console.log(c);
+let d = false;
+console.log(d);
+let e=1.5;
+console.log(e);
+// // let f = 10.000.000;
+// console.log(f);
+let f= "Data \"hello\"types";
+console.log(f);
